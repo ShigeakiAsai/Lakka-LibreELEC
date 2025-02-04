e25e3eaada71503a8726b1886eecfc478b803cba
ShigeakiAsai <asai@cocoa.ocn.ne.jp>
2025-02-04 18:34:15 +0900

    rocknix-joypad: Add rocknix-joypad driver package for RK356X

---
+++ projects/Rockchip/devices/RK356X/packages/rocknix-joypad/package.mk
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="rocknix-joypad"
+PKG_VERSION="e205fe5489561d955dbf00a4e24e73410cdd30f2"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/ROCKNIX/rocknix-joypad"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_LONGDESC="rocknix-joypad: ROCKNIX joypad driver"
+PKG_TOOLCHAIN="manual"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  kernel_make -C $(kernel_path) M=${PKG_BUILD}
+}
+
+makeinstall_target() {
+  mkdir -pv ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp -v *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+}
