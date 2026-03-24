a881f500a9bb43f9470085ea2ad7802816046011
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2026-03-24 09:15:28 +0100

    applewin: add libretro core
    Apple II emulator

---
+++ packages/lakka/libretro_cores/applewin/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="applewin"
+PKG_VERSION="e8eca6285c8f6a239959cc204760204e0bc17b1c"
+PKG_LICENSE="GPL-2.0"
+PKG_SITE="https://github.com/audetto/AppleWin"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Apple II emulator for Linux"
+PKG_TOOLCHAIN="cmake"
+
+PKG_CMAKE_OPTS_TARGET="-DBUILD_LIBRETRO=ON"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v source/frontends/libretro/applewin_libretro.so ${INSTALL}/usr/lib/libretro/
+}
