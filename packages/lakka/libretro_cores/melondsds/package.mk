d88d3ec503505324d4ce329b6afa78c77628fb2b
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2026-03-25 23:26:26 +0100

    melondsds: update core + disable optimizations
    internal compiler error is hit when code is optimized

--- packages/lakka/libretro_cores/melondsds/package.mk
+++ packages/lakka/libretro_cores/melondsds/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="melondsds"
-PKG_VERSION="0d65bd473d2dfda180fab7444664d76a4159350a"
+PKG_VERSION="bac0256dc6a8736c5a228f57c562257e45fd49f3"
 PKG_LICENSE="GPLv3"
 PKG_ARCH="aarch64 x86_64"
 PKG_SITE="https://github.com/JesseTG/melonds-ds"
@@ -7,6 +7,7 @@ PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A remake of the libretro melonDS core that prioritizes standalone parity, reliability, and usability."
 PKG_TOOLCHAIN="cmake"
+PKG_BUILD_FLAGS="+lto-off"
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_JIT=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5"
 
@@ -27,9 +28,6 @@ fi
 pre_make_target() {
   find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
   find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
-
-  # copy embedded bios files, as cmake / ninja fails to generate them
-  cp -r ${PKG_DIR}/embedded ${PKG_BUILD}/.${TARGET_NAME}/src/libretro
 }
 
 makeinstall_target() {
