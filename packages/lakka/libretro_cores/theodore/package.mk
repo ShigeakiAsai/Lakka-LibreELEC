88b1fb8e5da08ca852caff1ac82d3e8942bb141b
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-05-06 17:22:02 +0900

    'theodore: bump version to 121ae2513d3ee29f0aaf765a64dc086d57e7a4c7'

--- packages/lakka/libretro_cores/theodore/package.mk
+++ packages/lakka/libretro_cores/theodore/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="theodore"
-PKG_VERSION="c91ef6dd1aae4be32f70fb9d912b3221f7e44745"
+PKG_VERSION="121ae2513d3ee29f0aaf765a64dc086d57e7a4c7"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/Zlika/theodore"
 PKG_URL="${PKG_SITE}.git"
