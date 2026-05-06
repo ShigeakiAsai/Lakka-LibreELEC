73ef1d2ec44f704d044c6ce326c47d338e852dff
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-05-06 17:23:55 +0900

    'yabause: bump version to 7cb15b8f9eea5a6fa7cae34468a6989522bcba75'

--- packages/lakka/libretro_cores/yabause/package.mk
+++ packages/lakka/libretro_cores/yabause/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="yabause"
-PKG_VERSION="65af22e96beb6d9b0b9a50a81a39c86a6d604c1c"
+PKG_VERSION="7cb15b8f9eea5a6fa7cae34468a6989522bcba75"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/yabause"
 PKG_URL="${PKG_SITE}.git"
