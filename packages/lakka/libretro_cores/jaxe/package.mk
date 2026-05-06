9e33fc5104253f0c2ca44aa886da76853eeddd26
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-05-06 16:03:50 +0900

    jaxe: bump version to 581befc5d7273abc20ea1b137744f414aa70592c

--- packages/lakka/libretro_cores/jaxe/package.mk
+++ packages/lakka/libretro_cores/jaxe/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="jaxe"
-PKG_VERSION="e8e90e3d683bb560df5882f0ad62ed28f96a541a"
+PKG_VERSION="581befc5d7273abc20ea1b137744f414aa70592c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/kurtjd/jaxe"
 PKG_URL="${PKG_SITE}.git"
