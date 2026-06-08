3a696399189ca834be7b7ccc6fd75089ef82aeca
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-06-08 12:09:07 +0900

    libopenaptx: add libopenaptx package

---
+++ packages/audio/libopenaptx/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libopenaptx"
+PKG_VERSION="0.2.1"
+PKG_SHA256="a19b3b642def6f86441b73dd4efda6e6f13d49e7e2d6e5e91cc7eb464ebb505a"
+PKG_ARCH="any"
+PKG_LICENSE="GPL-3.0 license"
+PKG_SITE="https://github.com/pali/libopenaptx"
+PKG_URL="${PKG_SITE}/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Open Source implementation of Audio Processing Technology codec (aptX)"
+PKG_TOOLCHAIN="make"
