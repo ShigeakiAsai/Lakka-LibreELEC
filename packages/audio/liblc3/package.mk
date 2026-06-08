04e717a9eadb2fed860c564c5d6e9ff2be716c10
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-06-08 12:09:55 +0900

    liblc3: add liblc3 package

---
+++ packages/audio/liblc3/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="liblc3"
+PKG_VERSION="1.1.3"
+PKG_SHA256="276752ff54ce6a77d54ec133397b9d7e71f90caf3d9afa32d8b0e891b8ecb8af"
+PKG_ARCH="any"
+PKG_LICENSE="Apache-2.0 license"
+PKG_SITE="https://github.com/google/liblc3"
+PKG_URL="${PKG_SITE}/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Low Complexity Communication Codec (LC3)"
+PKG_TOOLCHAIN="meson"
