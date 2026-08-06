09936c8fde7cfec7b8ab47c2e5d2cbd6475e8064
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 15:09:21 +0900

    vbam: bump version to c97e7defbca0d10baffce0368900de1050a1176e
    and add patch file
    - 0001-vbam-libretro-skip-vbam-sdl-motion-when-SDL-and-WX-are-off.patch

--- packages/lakka/libretro_cores/vbam/package.mk
+++ packages/lakka/libretro_cores/vbam/package.mk
@@ -1,15 +1,43 @@
 PKG_NAME="vbam"
-PKG_VERSION="fdeaa840a5cd7b17a6f18ae9b8f9a55f02816668"
+PKG_VERSION="c97e7defbca0d10baffce0368900de1050a1176e"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/visualboyadvance-m/visualboyadvance-m"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="VBA-M with libretro integration"
-PKG_TOOLCHAIN="make"
+PKG_TOOLCHAIN="cmake"
 
-PKG_MAKE_OPTS_TARGET="-C ../src/libretro/"
+PKG_CMAKE_OPTS_TARGET=" -DCMAKE_BUILD_TYPE=Release \
+                        -DENABLE_SDL=OFF \
+                        -DENABLE_WX=OFF \
+                        -DENABLE_LINK=OFF \
+                        -DENABLE_LIBRETRO=ON \
+                        -DENABLE_LUA=OFF \
+                        -DENABLE_ONLINEUPDATES=OFF \
+                        -DENABLE_WAYLAND_PROTOCOLS=OFF"
+
+if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  PKG_CMAKE_OPTS_TARGET+=" -DDISABLE_OPENGL=OFF"
+elif [ "${OPENGL_SUPPORT}" = "no" ]; then
+  PKG_CMAKE_OPTS_TARGET+=" -DDISABLE_OPENGL=ON"
+fi
+
+if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  PKG_CMAKE_OPTS_TARGET+=" -DENABLE_GLES=ON"
+elif [ "${OPENGLES_SUPPORT}" = "no" ]; then
+  PKG_CMAKE_OPTS_TARGET+=" -DENABLE_GLES=OFF"
+fi
+
+if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${VULKAN}"
+  PKG_CMAKE_OPTS_TARGET+=" -DENABLE_VULKAN=ON"
+elif [ "${VULKAN_SUPPORT}" = "no" ]; then
+  PKG_CMAKE_OPTS_TARGET+=" -DENABLE_VULKAN=OFF"
+fi
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../src/libretro/vbam_libretro.so ${INSTALL}/usr/lib/libretro/
+  mkdir -p "${INSTALL}/usr/lib/libretro"
+    cp -av "${PKG_BUILD}/.${TARGET_NAME}/vbam_libretro.so" "${INSTALL}/usr/lib/libretro/"
 }
