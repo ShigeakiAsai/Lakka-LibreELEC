0e149edd8c89d30aff4174652b5998134157e984
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 15:09:12 +0900

    tgbdual: bump version to bf816b096f1dca55ea805337d7c9e78d6b98d839

--- packages/lakka/libretro_cores/tgbdual/package.mk
+++ packages/lakka/libretro_cores/tgbdual/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="tgbdual"
-PKG_VERSION="12540f0b2d3783259a0dce34ac8aa7a86beeaa11"
+PKG_VERSION="bf816b096f1dca55ea805337d7c9e78d6b98d839"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/tgbdual-libretro"
 PKG_URL="${PKG_SITE}.git"
