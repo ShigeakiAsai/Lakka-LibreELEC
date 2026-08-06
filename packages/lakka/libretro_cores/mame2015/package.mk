0f3871204c1abd5e76db61a1b71d5bfb9c187b32
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 13:01:12 +0900

    mame2015: bump version to 283e04b03c121db9be12632d6bad2fc3e8707248
    and removes upstreamed patch file
    - packages/lakka/libretro_cores/mame2015/patches/mame2015-python_3.patch
      https://github.com/libretro/mame2015-libretro/commit/a9e18fb71ce829b5ddfa87150c8fe570be2d8168

--- packages/lakka/libretro_cores/mame2015/package.mk
+++ packages/lakka/libretro_cores/mame2015/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="mame2015"
-PKG_VERSION="b0cd015f4de7a3979374ab70613db65415e99477"
+PKG_VERSION="283e04b03c121db9be12632d6bad2fc3e8707248"
 PKG_ARCH="x86_64 aarch64 arm"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2015-libretro"
