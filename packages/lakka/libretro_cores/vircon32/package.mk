b1f2875d519e191aad9f35a33f6a6f40e6e28329
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 15:09:25 +0900

    vircon32: bump version to 3a1b4ae3fb75c1216fab9cc8715e9a122c461a3a

--- packages/lakka/libretro_cores/vircon32/package.mk
+++ packages/lakka/libretro_cores/vircon32/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="vircon32"
-PKG_VERSION="3faedc6c577333eb785a93cec96ad7d484309f3c"
+PKG_VERSION="3a1b4ae3fb75c1216fab9cc8715e9a122c461a3a"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/vircon32/vircon32-libretro"
 PKG_URL="${PKG_SITE}.git"
