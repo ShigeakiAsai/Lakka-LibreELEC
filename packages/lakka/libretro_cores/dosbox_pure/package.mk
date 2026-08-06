c0757b47de86eac1f899e9833da7adf9127147a4
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 12:51:50 +0900

    dosbox_pure: bump version to a4a0bab7f8931433588f2fcad9045c85b277373d

--- packages/lakka/libretro_cores/dosbox_pure/package.mk
+++ packages/lakka/libretro_cores/dosbox_pure/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="dosbox_pure"
-PKG_VERSION="42485508b705e215d161eb581dec1984551fa9c2"
+PKG_VERSION="a4a0bab7f8931433588f2fcad9045c85b277373d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/dosbox-pure"
 PKG_URL="${PKG_SITE}.git"
