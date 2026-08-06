4acdc0c0c2c427641798d079d520fa527bef26b6
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 12:51:29 +0900

    clownmdemu: bump version to 935d6fc060eb82172dac29e880fe9b877fbdb640

--- packages/lakka/libretro_cores/clownmdemu/package.mk
+++ packages/lakka/libretro_cores/clownmdemu/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="clownmdemu"
-PKG_VERSION="64d2df1e434994344963b6347a1f1c171148aad8"
+PKG_VERSION="935d6fc060eb82172dac29e880fe9b877fbdb640"
 PKG_LICENSE="AGPLv3"
 PKG_SITE="https://github.com/Clownacy/clownmdemu-libretro"
 PKG_URL="${PKG_SITE}.git"
