6c942eb4c5a072792e8dac09882a07df2a30aa08
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-06 15:08:07 +0900

    same_cdi: bump version to 8a3c2acdbeb57aea4e74aeadaa703fdc5c9c5de9
    and changes PKG_MAKE_OPTS_TARGET with removing 3 patch files
    - https://github.com/libretro/same_cdi/commit/68a99910e8a534fd4d9bfe971acf4eb0e16240df
      NOTE: upstream commit 68a99910 ("Remove GENie/regenie build system;
            build with static Makefile.common") replaced the old GENie-generated
            build with a plain, self-contained Makefile.libretro + Makefile.common.
            None of the old GENie-era flags (REGENIE, CONFIG, LIBRETRO_OS, OSD,
            TOOLS, RETRO, CROSS_BUILD, PROJECT, LIBRETRO_CPU, PYTHON_EXECUTABLE,
            USE_SYSTEM_LIB_*, NOASM, VERBOSE, NOWERROR, OPENMP, PLATFORM) are read
            by the new Makefile anymore, so they were dropped.
    
            Critically, the old TARGET=mame flag collided with Makefile.libretro's
            own TARGET variable (which it uses to name the output .so). Command-line
            make variables win over in-makefile ":=" assignments, so TARGET=mame
            silently overrode "TARGET := $(TARGET_NAME).so", causing the build to
            link an executable named "mame" instead of same_cdi_libretro.so.

--- packages/lakka/libretro_cores/same_cdi/package.mk
+++ packages/lakka/libretro_cores/same_cdi/package.mk
@@ -1,38 +1,35 @@
 PKG_NAME="same_cdi"
-PKG_VERSION="2184aa6d87a31fb6c64534b9b7b2d26e36bae757"
+PKG_VERSION="8a3c2acdbeb57aea4e74aeadaa703fdc5c9c5de9"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/libretro/same_cdi"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain expat zlib flac sqlite"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="SAME_CDI is a libretro core to play CD-i games. This is a fork and modification of the MAME libretro core"
 PKG_TOOLCHAIN="make"
 
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro REGENIE=1 VERBOSE=1 NOWERROR=1 OPENMP=0 CROSS_BUILD=1 TOOLS=0 RETRO=1 PYTHON_EXECUTABLE=${TOOLCHAIN}/bin/${PKG_PYTHON_VERSION} CONFIG=libretro LIBRETRO_OS=unix TARGET=mame OSD=retro USE_SYSTEM_LIB_EXPAT=1 USE_SYSTEM_LIB_ZLIB=1 USE_SYSTEM_LIB_FLAC=1 USE_SYSTEM_LIB_SQLITE3=1 LIBRETRO_CPU= ARCH= PROJECT="
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro platform=unix"
 
 case ${ARCH} in
   x86_64)
-    PKG_MAKE_OPTS_TARGET+=" NOASM=0 PTR64=1 PLATFORM=x86_64"
+    PKG_MAKE_OPTS_TARGET+=" PTR64=1"
     ;;
   i386)
-    PKG_MAKE_OPTS_TARGET+=" NOASM=0 PTR64=0 PLATFORM=x86"
+    PKG_MAKE_OPTS_TARGET+=" PTR64=0"
     ;;
   aarch64)
-    PKG_MAKE_OPTS_TARGET+=" NOASM=0 PTR64=0 PLATFORM=arm64"
+    PKG_MAKE_OPTS_TARGET+=" PTR64=1"
     ;;
   arm)
-    PKG_MAKE_OPTS_TARGET+=" NOASM=1 PTR64=0 PLATFORM=arm"
+    PKG_MAKE_OPTS_TARGET+=" PTR64=0"
     ;;
 esac
 
 pre_make_target() {
-  PKG_MAKE_OPTS_TARGET+=" OVERRIDE_CC=${CC} OVERRIDE_CXX=${CXX} OVERRIDE_LD=${LD}"
-  sed -i scripts/genie.lua \
-      -e 's|-static-libstdc++||g'
+  PKG_MAKE_OPTS_TARGET+=" CC=${CC} CXX=${CXX}"
 }
 
 make_target() {
   unset DISTRO
-  [ "${ARCH}" = "aarch64" ] && export ARCHOPTS="-D__aarch64__ -DASMJIT_BUILD_X86"
   make ${PKG_MAKE_OPTS_TARGET}
 }
 
