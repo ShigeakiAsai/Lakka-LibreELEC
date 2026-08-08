b3472ab56d396340cb05c46a26a64baf028d79dd
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-08 13:05:54 +0900

    retroflag_picase_safeshutdown: enable also RPi3/4/5 Composite

--- packages/lakka/lakka_tools/retroflag_picase_safeshutdown/package.mk
+++ packages/lakka/lakka_tools/retroflag_picase_safeshutdown/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="retroflag_picase_safeshutdown"
-PKG_VERSION="1.0"
+PKG_VERSION="1.1"
 PKG_ARCH="aarch64"
 PKG_DEPENDS_TARGET="Python3 lg-gpio"
 PKG_SECTION="system"
@@ -8,19 +8,19 @@ PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p "${INSTALL}/usr/bin"
-    if [ "${DEVICE}" = "RPi3" -o "${DEVICE}" = "RPi4" ]; then
+    if [ "${DEVICE:0:4}" = "RPi3" -o "${DEVICE}" = "RPi4" -o "${DEVICE}" = "RPi4-Composite" ]; then
       cp -av "${PKG_DIR}/scripts/retroflag_picase_safeshutdown.py" "${INSTALL}/usr/bin"
       cp -av "${PKG_DIR}/scripts/retroflag_picase_check_gpio-poweroff_overlay.sh" "${INSTALL}/usr/bin"
       cp -av "${PKG_DIR}/scripts/retroflag_picase_install_gpio-poweroff_overlay.sh" "${INSTALL}/usr/bin"
-    elif [ "${DEVICE}" = "RPi5" ]; then
+    elif [ "${DEVICE:0:4}" = "RPi5" ]; then
       cp -av "${PKG_DIR}/scripts/retroflag_picase_safeshutdown_pi5.py" "${INSTALL}/usr/bin"
     fi
 }
 
 post_install() {
-  if [ "${DEVICE}" = "RPi3" -o "${DEVICE}" = "RPi4" ]; then
+  if [ "${DEVICE:0:4}" = "RPi3" -o "${DEVICE}" = "RPi4" -o "${DEVICE}" = "RPi4-Composite" ]; then
     enable_service retroflag_picase_safeshutdown.service
-  elif [ "${DEVICE}" = "RPi5" ]; then
+  elif [ "${DEVICE:0:4}" = "RPi5" ]; then
     enable_service retroflag_picase_safeshutdown_pi5.service
   fi
 }
