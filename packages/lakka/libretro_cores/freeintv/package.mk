b274fe7c305afe37120afddd6eb3a6962eeb9477
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:19:15 +0900

    freeintv: bump version to ef3e0fe322bec62a7f916c0bb0834c08c348d0b4

--- packages/lakka/libretro_cores/freeintv/package.mk
+++ packages/lakka/libretro_cores/freeintv/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="freeintv"
-PKG_VERSION="df5a5312985b66b1ec71b496868641e40b7ad1c9"
+PKG_VERSION="ef3e0fe322bec62a7f916c0bb0834c08c348d0b4"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/FreeIntv"
 PKG_URL="${PKG_SITE}.git"
