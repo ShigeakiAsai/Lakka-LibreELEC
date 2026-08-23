c8dc2d1d185c1ee12c3ebfad5c0b9858b51eed3e
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:18:57 +0900

    ep128emu: bump version to 6af5d03de220e6effcd0ffcd99e0fb8e8c77cc9e

--- packages/lakka/libretro_cores/ep128emu/package.mk
+++ packages/lakka/libretro_cores/ep128emu/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="ep128emu"
-PKG_VERSION="812dd1b35225b64d9392a3e21de28c3a5861ddf7"
+PKG_VERSION="6af5d03de220e6effcd0ffcd99e0fb8e8c77cc9e"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/ep128emu-core"
 PKG_URL="${PKG_SITE}.git"
