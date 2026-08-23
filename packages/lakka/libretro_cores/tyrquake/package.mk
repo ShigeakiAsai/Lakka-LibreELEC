28a8592baecf2de11d950d5d2753fc035ab6a019
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:32:02 +0900

    tyrquake: bump version to e57bb11597e8a00380f30f2627d219da960cf69a
    and remove the upstreamed patch file
    - packages/lakka/libretro_cores/tyrquake/patches/tyrquake-fix_qbtypes_header.patch
      https://github.com/libretro/tyrquake/commit/7da7b688762df7a5e05a47edad80b53da7216a28

--- packages/lakka/libretro_cores/tyrquake/package.mk
+++ packages/lakka/libretro_cores/tyrquake/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="tyrquake"
-PKG_VERSION="471f2a72d2a7b0416f6a21eb945e5af831df06a4"
+PKG_VERSION="e57bb11597e8a00380f30f2627d219da960cf69a"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/tyrquake"
 PKG_URL="${PKG_SITE}.git"
