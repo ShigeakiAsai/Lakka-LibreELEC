aca78a40c0c855b03e8e6d1b3f4f88e15eb46460
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:28:48 +0900

    openlara: bump version to e4ce52edec5a9a6ad22b69d08c687bf070e68876

--- packages/lakka/libretro_cores/openlara/package.mk
+++ packages/lakka/libretro_cores/openlara/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="openlara"
-PKG_VERSION="72facbeaa33502af342e3c8f90ce0ccd79dbc765"
+PKG_VERSION="e4ce52edec5a9a6ad22b69d08c687bf070e68876"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/libretro/openlara"
 PKG_URL="${PKG_SITE}.git"
