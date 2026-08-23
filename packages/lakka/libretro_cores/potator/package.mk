20525d52dfd4f134e81e3775fa16738dfc642848
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:30:25 +0900

    potator: bump version to 227c5f6f3ce74d32e9002ce24c1420288559a860

--- packages/lakka/libretro_cores/potator/package.mk
+++ packages/lakka/libretro_cores/potator/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="potator"
-PKG_VERSION="ad87bc6068ef126e48339b440465fb0bf5a2794f"
+PKG_VERSION="227c5f6f3ce74d32e9002ce24c1420288559a860"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/libretro/potator"
 PKG_URL="${PKG_SITE}.git"
