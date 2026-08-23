90ff2d2837ac9ede5864193584c1367f4d795b08
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:19:12 +0900

    fmsx: bump version to f013e213458e06d9df718e4bc4b09d46f88aa899

--- packages/lakka/libretro_cores/fmsx/package.mk
+++ packages/lakka/libretro_cores/fmsx/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="fmsx"
-PKG_VERSION="fbe4dfc4c3e3f7eb27089def3d663a905b181845"
+PKG_VERSION="f013e213458e06d9df718e4bc4b09d46f88aa899"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/fmsx-libretro"
 PKG_URL="${PKG_SITE}.git"
