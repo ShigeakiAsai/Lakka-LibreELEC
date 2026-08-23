bd5d5750339384a47c48088c482fd061802f20ea
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:18:48 +0900

    dosbox_svn: bump version to 752aec34390694527f4471fdabf58ee282573b2b

--- packages/lakka/libretro_cores/dosbox_svn/package.mk
+++ packages/lakka/libretro_cores/dosbox_svn/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="dosbox_svn"
-PKG_VERSION="53ca2f6303a652d129321cfc521f000cd7ec5531"
+PKG_VERSION="752aec34390694527f4471fdabf58ee282573b2b"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/dosbox-svn"
 PKG_URL="${PKG_SITE}.git"
