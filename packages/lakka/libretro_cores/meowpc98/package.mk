bc4efb945d935f116345929187601719f1fed8b1
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:27:59 +0900

    meowpc98: bump version to 5fdbb2187f6b14ca7bf668dd52b36c820b989995

--- packages/lakka/libretro_cores/meowpc98/package.mk
+++ packages/lakka/libretro_cores/meowpc98/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="meowpc98"
-PKG_VERSION="dc905d4e10470ff65a38e8b9e1a75b43b9b12149"
+PKG_VERSION="5fdbb2187f6b14ca7bf668dd52b36c820b989995"
 PKG_LICENSE="Unknown"
 PKG_SITE="https://github.com/libretro/libretro-meowPC98"
 PKG_URL="${PKG_SITE}.git"
