cc5a27f9fbd64a6c0c9c449cdbef8b2b6591be84
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:28:46 +0900

    o2em: bump version to 679d6fec04963f6e70a7ec217e3d0ebb1fe472fc

--- packages/lakka/libretro_cores/o2em/package.mk
+++ packages/lakka/libretro_cores/o2em/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="o2em"
-PKG_VERSION="3ba4231c1dc8dcdf487428712856b790d2e4b8f3"
+PKG_VERSION="679d6fec04963f6e70a7ec217e3d0ebb1fe472fc"
 PKG_LICENSE="Artistic License"
 PKG_SITE="https://github.com/libretro/libretro-o2em"
 PKG_URL="${PKG_SITE}.git"
