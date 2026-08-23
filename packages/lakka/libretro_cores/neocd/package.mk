907ce03323f5b73875b1cc841f1861efa6986ea0
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:28:32 +0900

    neocd: bump version to 331cb2c4aa3d8490ec83dacf7c3650f13d7c3bf5

--- packages/lakka/libretro_cores/neocd/package.mk
+++ packages/lakka/libretro_cores/neocd/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="neocd"
-PKG_VERSION="5eca2c8fd567b5261251c65ecafa8cf5b179d1d2"
+PKG_VERSION="331cb2c4aa3d8490ec83dacf7c3650f13d7c3bf5"
 PKG_LICENSE="LGPLv3"
 PKG_SITE="https://github.com/libretro/neocd_libretro"
 PKG_URL="${PKG_SITE}.git"
