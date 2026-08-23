72fc197b2ea1454a79a6062ff9a945cf66b4e5a7
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:28:12 +0900

    mgba_fork: bump version to e31759b24e7a4e3899285ff720d7b573ac328ae7
    and changes PKG_TOOLCHAIN to "cmake" with a patch file
    - https://github.com/libretro/mgba/commit/89404771c940255233f96829289407e8b4f5d97d
      NOTE: upstream commit 89404771c ("Change build system for libretro
            gitlab CI from make to cmake") removed Makefile.libretro and folded the
            libretro core build into the main CMakeLists.txt behind BUILD_LIBRETRO.
    
            The mgba_libretro CMake target's own COMPILE_DEFINITIONS/link list never
            propagates USE_LIBZIP/USE_MINIZIP or ${ZIP_LIBRARIES} (unlike the main
            libmgba library), so src/util/vfs/vfs-zip.c always falls into its
            "#else" branch and expects <minizip/zip.h> -- which Lakka no longer
            packages. Patched in patches/ to define USE_LIBZIP and link
            ${ZIP_LIBRARIES} for the libretro target specifically.

--- packages/lakka/libretro_cores/mgba_fork/package.mk
+++ packages/lakka/libretro_cores/mgba_fork/package.mk
@@ -1,23 +1,44 @@
 PKG_NAME="mgba_fork"
-PKG_VERSION="c758314a639aa0066e7b65a8341448181b73c804"
+PKG_VERSION="e31759b24e7a4e3899285ff720d7b573ac328ae7"
 PKG_LICENSE="MPLv2.0"
 PKG_SITE="https://github.com/libretro/mgba"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="mGBA Game Boy Advance Emulator"
-PKG_TOOLCHAIN="make"
+PKG_TOOLCHAIN="cmake"
 
-PKG_MAKE_OPTS_TARGET="-C ../ -f Makefile.libretro"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+                       -DBUILD_LIBRETRO=ON \
+                       -DSKIP_LIBRARY=ON \
+                       -DBUILD_QT=OFF \
+                       -DBUILD_SDL=OFF \
+                       -DUSE_DISCORD_RPC=OFF \
+                       -DUSE_EDITLINE=OFF \
+                       -DUSE_EPOXY=OFF \
+                       -DUSE_JSON_C=OFF \
+                       -DUSE_LUA=OFF \
+                       -DUSE_MINIZIP=OFF"
 
 if [ "${OPENGL_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GL=ON"
+else
+  PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GL=OFF"
 fi
 
 if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+
+  get_graphicdrivers
+
+  if listcontains "${GRAPHIC_DRIVERS}" "(panfrost|vc4)" && ! listcontains "${MALI_FAMILY}" "t720"; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GLES3=ON -DBUILD_GLES2=OFF"
+  else
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GLES3=OFF -DBUILD_GLES2=ON"
+  fi
 fi
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../mgba_libretro.so ${INSTALL}/usr/lib/libretro/mgba_fork_libretro.so
+    cp -v "${PKG_BUILD}/.${TARGET_NAME}/mgba_libretro.so" ${INSTALL}/usr/lib/libretro/mgba_fork_libretro.so
 }
