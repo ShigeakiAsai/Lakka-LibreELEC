9e2a9af464bbec0e44fc1023c609fd62b92c97f3
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:24:30 +0900

    geargrafx: bump version to ff264f1e9f9f7244e3ef44ab722d75157d3a5448

--- packages/lakka/libretro_cores/geargrafx/package.mk
+++ packages/lakka/libretro_cores/geargrafx/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="geargrafx"
-PKG_VERSION="14b1cda0ba339e5346073a4d9cc47bf4cbaef524"
+PKG_VERSION="ff264f1e9f9f7244e3ef44ab722d75157d3a5448"
 PKG_LICENSE="GPL-3.0"
 PKG_SITE="https://github.com/drhelius/Geargrafx"
 PKG_URL="${PKG_SITE}.git"
