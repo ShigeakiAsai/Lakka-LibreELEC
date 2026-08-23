efca07397de2b3092ef4187b56868364d6255e78
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-08-23 22:30:52 +0900

    quicknes: bump version to 26bb785c9deddb66a17717b21bb4e328f03ade32

--- packages/lakka/libretro_cores/quicknes/package.mk
+++ packages/lakka/libretro_cores/quicknes/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="quicknes"
-PKG_VERSION="dbf19f73e3eb9701d1c7f5898f57c097e05c9fbd"
+PKG_VERSION="26bb785c9deddb66a17717b21bb4e328f03ade32"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://github.com/libretro/QuickNES_Core"
 PKG_URL="${PKG_SITE}.git"
